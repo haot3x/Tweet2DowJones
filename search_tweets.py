d9f6054eb62b9e81ab5baf07aac0e3c0c0598db9
Haotian <tedsunnyday@gmail.com>
2014-03-27 22:23:01 -0400

    sqlite3 added

--- search_tweets.py
+++ search_tweets.py
@@ -1,11 +1,9 @@
 from TwitterAPI import TwitterAPI,TwitterOAuth
-
+import sqlite3
+conn = sqlite3.connect('test.db')
 
 SEARCH_TERM = 'pizza'
-
-
 o = TwitterOAuth.read_file()
-
 # Using OAuth1...
 api = TwitterAPI(
                  o.consumer_key,
@@ -15,7 +13,25 @@ api = TwitterAPI(
 
 r = api.request('search/tweets', {'q': SEARCH_TERM})
 
+#https://dev.twitter.com/docs/api/1.1/get/search/tweets
+#
+# create table test_tweet(created_at TEXT, text TEXT);
+
+#
+#
+#
+#
+cnt = 0
 for item in r:
+    cnt += 1
     print(item['text'] if 'text' in item else item)
+    print(item['created_at'])
+    text = item['text']
+    created_at = item['created_at']
+    conn.execute('INSERT INTO test_tweet VALUES (?,?)',(created_at,text))
+    
+conn.commit()
+conn.close()
+print('\n DATABASE commited %d items inserted\n' % (cnt,))
 
 print('\nQUOTA: %s' % r.get_rest_quota())
